615c198290e659f1d4f246eec1c067c11d456f39
Tristan <tristan@qq.com>
2024-05-18 18:47:02 +0800

    带头结点链表

---
+++ headNodeLinkedList/headNodeLinkedList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  headNodeLinkedList
+//
+//  Created by 曾长欢 on 2024/5/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
